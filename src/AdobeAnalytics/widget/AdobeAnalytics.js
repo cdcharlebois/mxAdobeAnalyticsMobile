358ec2e6c45c2c1caa9fc285181a757e60361f1b
Conner Charlebois <cdcharlebois@gmail.com>
2017-08-24 16:56:34 -0400

    init

---
+++ src/AdobeAnalytics/widget/AdobeAnalytics.js
@@ -0,0 +1,80 @@
+define([
+    "dojo/_base/declare",
+    "mxui/widget/_WidgetBase",
+
+    "mxui/dom",
+    "dojo/dom",
+    "dojo/dom-prop",
+    "dojo/dom-geometry",
+    "dojo/dom-class",
+    "dojo/dom-style",
+    "dojo/dom-construct",
+    "dojo/_base/array",
+    "dojo/_base/lang",
+    "dojo/text",
+    "dojo/html",
+    "dojo/_base/event",
+    "widgets/AdobeAnalytics/widget/lib/ADB_Helper"
+
+
+], function(declare, _WidgetBase, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml, dojoEvent, ADB) {
+    "use strict";
+
+    return declare("AdobeAnalytics.widget.AdobeAnalytics", [_WidgetBase], {
+
+
+        // Internal variables.
+        _handles: null,
+        _contextObj: null,
+
+        constructor: function() {
+            this._handles = [];
+            //check for the cordova library and the plugin
+            // TODO:
+            // add the json config file
+            console.log(ADB);
+            console.log(`typeof ADB.trackState is ${typeof ADB.trackState}`)
+            console.log(`calling ADB.trackState...`)
+            ADB.trackState('test event', { foo: 'bar' })
+        },
+
+        postCreate: function() {
+            logger.debug(this.id + ".postCreate");
+        },
+
+        update: function(obj, callback) {
+            logger.debug(this.id + ".update");
+
+            this._contextObj = obj;
+            this._updateRendering(callback);
+        },
+
+        resize: function(box) {
+            logger.debug(this.id + ".resize");
+        },
+
+        uninitialize: function() {
+            logger.debug(this.id + ".uninitialize");
+        },
+
+        _updateRendering: function(callback) {
+            logger.debug(this.id + "._updateRendering");
+
+            if (this._contextObj !== null) {
+                dojoStyle.set(this.domNode, "display", "block");
+            } else {
+                dojoStyle.set(this.domNode, "display", "none");
+            }
+
+            this._executeCallback(callback);
+        },
+
+        _executeCallback: function(cb) {
+            if (cb && typeof cb === "function") {
+                cb();
+            }
+        }
+    });
+});
+
+require(["AdobeAnalytics/widget/AdobeAnalytics"]);
